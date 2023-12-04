da962547550d6a9e7ae01d5126e45713ce2eca94
acascoc098 <acascoc098@g.educaand.es>
2023-12-04 11:46:04 +0100

    Subida

---
+++ app/src/main/java/com/example/fragmentos/FirstFragment.kt
@@ -0,0 +1,41 @@
+package com.example.fragmentos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fragmentos.databinding.ActivityMainBinding
+import com.example.fragmentos.databinding.FragmentBlankBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FirstFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FirstFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private lateinit var binding : FragmentBlankBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentBlankBinding.inflate(layoutInflater)
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.TVFrag.text = "Andrea"
+    }
+}
